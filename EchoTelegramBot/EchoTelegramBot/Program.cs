1c5850a5fc7bd11f3f599c4b4b8e0e28bd418f93
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2026-02-12 22:37:40 +0500

    Add files via upload
    Updated EchoBot

---
+++ EchoTelegramBot/EchoTelegramBot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Xsl;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using EchoTelegramBot;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Dastur ishga tushdi..");
+        string botToken = "8433044942:AAFyo8iIJpwx7de0P-AUBHOnR2TrqGtC6dc";
+        var bot = new EchoBot(botToken);
+        await bot.StartAsync();
+    }
+}
---
+++ EchoTelegramBot/EchoTelegramBot/src/EchoBot.cs
@@ -0,0 +1,46 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace EchoTelegramBot;
+
+public class EchoBot
+{
+    private readonly ITelegramBotClient botClient;
+    int numberOfUpdate = 0;
+
+    public EchoBot(string botToken)
+    {
+        botClient = new TelegramBotClient(botToken);
+    }
+
+    public async Task StartAsync()
+    {
+        while (true)
+        {
+            try
+            {
+                var updates = await botClient.GetUpdates(numberOfUpdate, timeout: 30);
+
+                foreach (var update in updates)
+                {
+                    numberOfUpdate = update.Id + 1;
+
+                    var message = update.Message;
+                    if (update.Type != UpdateType.Message && string.IsNullOrEmpty(message.Text))
+                    {
+                        continue;
+                    }
+
+                    await botClient.SendMessage(
+                        message.Chat.Id,
+                        message.Text);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Xatolik: {e}");
+                await Task.Delay(2000);
+            }
+        }
+    }
+}

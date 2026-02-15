b1688fcc92194cd1947497ee018d738a834447f1
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2026-02-15 13:02:04 +0500

    Add files via upload
    bug fixed

--- EchoTelegramBot/EchoTelegramBot/Program.cs
+++ EchoTelegramBot/EchoTelegramBot/Program.cs
@@ -3,15 +3,49 @@ using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.Enums;
-using EchoTelegramBot;
+
 
 class Program
 {
     static async Task Main()
     {
-        Console.WriteLine("Dastur ishga tushdi..");
+        Console.WriteLine("Bot ishga tushdi..");
         string botToken = "8433044942:AAFyo8iIJpwx7de0P-AUBHOnR2TrqGtC6dc";
-        var bot = new EchoBot(botToken);
-        await bot.StartAsync();
+
+        using var cancellationTokenSource = new CancellationTokenSource();
+        var receiverOptions = new ReceiverOptions()
+        {
+            AllowedUpdates = new[] { UpdateType.Message },
+        };
+        var botClient = new TelegramBotClient(botToken);
+
+        if (HandlePollingErrorAsync != null)
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                errorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cancellationTokenSource.Token);
+        await Task.Delay(-1, cancellationTokenSource.Token);
+    }
+
+
+    static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        if (update.Message is  null)
+            return;
+        var chatId = update.Message.Chat.Id;
+        var messageText = update.Message.Text;
+        
+        await botClient.SendMessage(
+            chatId: chatId,
+            text: messageText,
+            cancellationToken: cancellationToken);
+    }
+
+    private static Task HandlePollingErrorAsync(ITelegramBotClient botClient,Exception error,CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Xatolik: {error}");
+        return Task.CompletedTask;
     }
 }
